56d1b390d94e78d51b879871d58e9a79f4e7d5fb
brushlessmiku <fangzhou.yu@stonybrook.edu>
2019-12-30 01:31:15 -0500

    added more drilling sequence logic

--- Drilling_Sequence.ino
+++ Drilling_Sequence.ino
@@ -2,7 +2,7 @@
 
 int drillMotorEndSpeed = 255;
 int drillMotorSpeed = 0;
-int drillMotorRampPeriod = 500; //time for drill to throttle up in millis 
+unsigned long drillMotorRampPeriod = 500; //time for drill to throttle up in millis 
 int numberOfDrillMotorThrottleSteps = 50; //number of drill motor throttle steps to take, needs to be a factor of drillMotorRampPeriod.
 int drillMotorRampInterval = drillMotorRampPeriod/numberOfDrillMotorThrottleSteps;
 
@@ -12,37 +12,60 @@ int drillMotorThrottleStep =  (int) drillMotorEndSpeed/numberOfDrillMotorThrottl
 
 unsigned long previousDrillRampMillis = 0; //timer for drill throttle ramping
 
+unsigned long previousCarrierPlateMillis = 0;
+
+int timeToBumpCarrierPlateUp = 1000; // length of time in millis to bump up carrier plate 
+
+
+bool haltMotorProcedureComplete = false; 
+bool carrierPlateBumpStatus = false;
+bool carrierPlateParkingProcedureComplete = false;
 void drillSequence(){
 
     //turn on drill and advance the carrier plate down until the bottom endstop is triggered.
     //when bottom endstop is triggered, turn off drill. Reverse linear actuator a short distance and wait a short moment. 
     //Reverse drill and cycle linear actuator up until top endstop is triggered.
     //set drilling_in_progress to false after triggering top endstop.       
-    if (cycleStartButtonState == 1 && endstopBotSwitchState == 0)
+    if (cycleStartButtonState == 1 && endstopBotSwitchState == 0){
 
-        {
-            cycleLinearAcuatorDown();
-            rampDrillMotorUp(drillMotorEndSpeed);
+        
+        cycleLinearActuatorDown();
+        rampDrillMotorUp(drillMotorEndSpeed);
 
     }
     
-    else{
-
+    if(cycleStartButtonState == 1 && endstopBotSwitchState == 1 && haltMotorProcedureComplete == false){
+        
         stopLinearActuator();
-        stopDrillMotor();
+        rampDrillMotorDown();
+ 
     }
+
+    if(cycleStartButtonState == 1 && endstopBotSwitchState == 1 && haltMotorProcedureComplete == true){
+        
+        bumpCarrierPlateUp(timeToBumpCarrierPlateUp);
+ 
+    }
+    
+    if(cycleStartButtonState == 1 && endstopBotSwitchState == 1 && carrierPlateParkingProcedureComplete == true){
+        
+        rampDrillMotorUp(); //spin drill motor CCW
+        cycleLinearActuatorUp();
+        
+    }
+
    
 }
 
-void rampDrillMotorUp(int endSpeed){
-    //provide a smooth ramp input to the drill motor 
+void rampDrillMotorUp(int topDrillSpeed){
+    //provide a smooth ramp up input to the drill motor 
     //should be non-blocking 
 
     if (currentMillis - previousDrillRampMillis >= drillMotorRampInterval){
         
-        previousDrillRampMillis += drillMotorRampInterval;
+        previousDrillRampMillis = currentMillis;
 
-        if(drillMotorSpeed < endSpeed){
+        if(drillMotorSpeed < topDrillSpeed){
 
             drillMotorSpeed += drillMotorThrottleStep;
 
@@ -50,15 +73,70 @@ void rampDrillMotorUp(int endSpeed){
 
         }else {
 
-            analogWrite(drillMotorPin, endSpeed);
+            analogWrite(drillMotorPin, topDrillSpeed);
         }
     }
 
 }
-void cycleLinearAcuatorDown(){
+
+void rampDrillMotorDown(){
+    //provide a smooth ramp down input to the drill motor 
+    //should be non-blocking 
+
+    if (currentMillis - previousDrillRampMillis >= drillMotorRampInterval){
+        
+        previousDrillRampMillis = currentMillis;
+
+        if(drillMotorSpeed >= 0){
+
+            drillMotorSpeed -= drillMotorThrottleStep;
+
+            analogWrite(drillMotorPin, drillMotorSpeed);
+
+        }else {
+
+            analogWrite(drillMotorPin, 0);
+            haltMotorProcedureComplete = true; 
+        }
+    }
+
+}
+
+void bumpCarrierPlateUp(int bumpTime){
+
+    //function used to "park" the carrier plate slightly above the bottom endstop switch 
+    //open loop 
+
+    if(currentMillis - previousCarrierPlateMillis >= bumpTime){
+
+        previousCarrierPlateMillis  = currentMillis;
+
+
+        if(carrierPlateBumpStatus == false){
+
+            analogWrite(linearActuatorMotorSignal, 15);
+            carrierPlateBumpStatus = true; 
+        }else {
+
+            analogWrite(linearActuatorMotorSignal, 0);
+            carrierPlateParkingProcedureComplete = true; 
+        }
+
+
+    }
+
+}
+
+void cycleLinearActuatorDown(){
 
     analogWrite(linearActuatorMotorSignal, 15);
     drilling_In_Progress  = true; 
+
+}
+
+void cycleLinearActuatorUp(){
+    
+    analogWrite(linearActuatorMotorSignal, 15);
 
 }
 
